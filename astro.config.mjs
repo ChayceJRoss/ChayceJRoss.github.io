3b49c9d7523cdb43affb10ac93686a1e3d472416
ChayceJRoss <chayceross108@gmail.com>
2024-12-20 11:41:46 -0800

    starting migration from scratch

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  markdown: {
-    syntaxHighlight: 'prism'
-  }
+  integrations: [tailwind()]
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,22 +1,8 @@
-import defaultTheme from 'tailwindcss/defaultTheme'
-
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {
-			fontFamily: {
-				sans: ['Inter Variable', ...defaultTheme.fontFamily.sans],
-				serif: ['Merriweather', ...defaultTheme.fontFamily.serif]
-			},
-			colors: {
-				"accent": "rgb(var(--accent))",
-				"primary": "rgb(var(--primary))",
-				"primary-light": "rgb(var(--primary-light))",
-				"accent-light": "rgb(var(--accent-light))",
-				"secondary": "rgb(var(--secondary))",
-			}
-		},
+		extend: {},
 	},
 	plugins: [],
 }
